63a5d4b1696fbf3c20d1f31e9745e453c2c82c77
Ryan Gallaway <rkgallaway@msn.com>
2021-08-21 12:52:20 -0700

    Lab 13 demos added

---
+++ class-13/inclass-demo/login-goodness/src/LogoutButton.js
@@ -0,0 +1,17 @@
+// this came from Auth) docs
+// https://auth0.com/docs/quickstart/spa/react#add-login-to-your-application
+
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
+const LogoutButton = () => {
+  const { logout } = useAuth0();
+
+  return (
+    <button onClick={() => logout({ returnTo: window.location.origin })}>
+      Log Out
+    </button>
+  );
+};
+
+export default LogoutButton;
---
+++ class-13/inclass-demo/login-goodness/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+import App from './App';
+import { Auth0Provider } from "@auth0/auth0-react";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Auth0Provider
+      domain="dev-t18s8k45.us.auth0.com"
+      clientId="EctF2wPXZYgj8FJcDiLjZsauEfHOFXs6"
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
